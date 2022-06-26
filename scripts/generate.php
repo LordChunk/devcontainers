be12341d1c36547cc7f65ba32735fea94c8af58e
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-26 15:31:54 +0200

    Update

--- scripts/generate.php
+++ scripts/generate.php
@@ -4,10 +4,16 @@ $configuration = $argv[1];
 $configurationPath = '../configurations/' . $configuration . '.json';
 $json = json_decode(file_get_contents($configurationPath), true);
 
-$template = file_get_contents('../templates/Dockerfile');
+$segments = [
+    'FROM' => $json['base_image'],
+    'EXTENSIONS' => implode(' ', $json['extensions']),
+];
 
-$dockerfile = str_replace('%FROM%', $json['from'], $template);
-$dockerfile = str_replace('%EXTENSIONS%', implode(' ', $json['extensions']), $template);
+$dockerfile = file_get_contents('../templates/Dockerfile');
+
+foreach ($segments as $segment => $value) {
+    $dockerfile = str_replace('%' . $segment . '%', $value, $dockerfile);
+}
 
 if (!file_exists('../dockerfile')) {
     mkdir('../dockerfile', 0777, true);
