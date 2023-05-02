9ffced8405c33686247a35eace6527efece962c1
Job <LordChunk@users.noreply.github.com>
2023-05-02 14:10:45 +0000

    Hacky workaround for supporting multiple build-args

--- laravel/scripts/generate.sh
+++ laravel/scripts/generate.sh
@@ -4,16 +4,27 @@ configuration="$1"
 configurationPath="../configurations/$configuration.json"
 env_file=$(
   jq -jr 'to_entries |
-    map("\(.key)=\(.value |
-      if type == "array" then
-        join(" ")
+    map(
+      if . == first then
+        "\(.key)=\(.value |
+          if type == "array" then
+            join(" ")
+          else
+            tostring
+          end)"
       else
-        tostring
-      end) ") |
-    .[]' "$configurationPath"
+        " --build-arg \(.key)=\(.value |
+          if type == "array" then
+            join(" ")
+          else
+            tostring
+          end)"
+      end
+    )' "$configurationPath"
 )
+
 
 dockerfile=$(cat "../templates/Dockerfile")
 
 # Write env file to name of config
-echo "$env_file" > "../env."$configuration
+echo "$env_file" >"../env."$configuration
