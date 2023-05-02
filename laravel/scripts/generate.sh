93f3a627b2c913ea26388ebc2b5c546d0b98a62f
Job <LordChunk@users.noreply.github.com>
2023-05-02 19:36:44 +0000

    Reverted changes to generate script

--- laravel/scripts/generate.sh
+++ laravel/scripts/generate.sh
@@ -1,30 +1,17 @@
 #!/bin/bash
-
 configuration="$1"
 configurationPath="../configurations/$configuration.json"
 env_file=$(
   jq -jr 'to_entries |
-    map(
-      if . == first then
-        "\(.key)=\(.value |
-          if type == "array" then
-            join(" ")
-          else
-            tostring
-          end)"
+    map("\(.key)=\(.value |
+      if type == "array" then
+        join(" ")
       else
-        " --build-arg \(.key)=\(.value |
-          if type == "array" then
-            join(" ")
-          else
-            tostring
-          end)"
-      end
-    )' "$configurationPath"
+        tostring
+      end)")' "$configurationPath"
 )
-
 
 dockerfile=$(cat "../templates/Dockerfile")
 
 # Write env file to name of config
-echo "$env_file" >"../env."$configuration
+echo "$env_file" > "../env."$configuration
