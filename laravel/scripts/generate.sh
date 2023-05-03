d623b9509cfe797ebd7de09c63423e858d6c0d95
Job <LordChunk@users.noreply.github.com>
2023-05-03 11:03:42 +0000

    Compacted json output

--- laravel/scripts/generate.sh
+++ laravel/scripts/generate.sh
@@ -2,7 +2,7 @@
 configuration="$1"
 configurationPath="../configurations/$configuration.json"
 env_file=$(
-  jq 'to_entries | map(if .value | type == "array" then {key: .key, value: (.value | join(" "))} else . end) | from_entries' "$configurationPath"
+  jq -c 'to_entries | map(if .value | type == "array" then {key: .key, value: (.value | join(" "))} else . end) | from_entries' "$configurationPath"
 )
 
 dockerfile=$(cat "../templates/Dockerfile")
