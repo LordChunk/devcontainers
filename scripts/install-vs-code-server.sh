92e4a8c9fbe95588217aff51c7db09d6e4a27ec8
LordChunk <LordChunk@users.noreply.github.com>
2024-05-06 20:35:14 +0000

    Updated vscode install script

--- scripts/install-vs-code-server.sh
+++ scripts/install-vs-code-server.sh
@@ -1,58 +1,75 @@
 #!/bin/sh
-# Script source: https://gist.github.com/b01/0a16b6645ab7921b0910603dfb85e4fb
-# Installs VS Code Server to ~/.vscode-server/bin/<commit-sha>
-# Commit sha is auto-detected from the latest release.
-# This script is meant to be run as a user, not root.
+
+# https://gist.github.com/b01/0a16b6645ab7921b0910603dfb85e4fb#file-download-vs-code-server-sh
+# Copyright 2023 Khalifah K. Shabazz
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the “Software”),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
 set -e
 
 # Auto-Get the latest commit sha via command line.
 get_latest_release() {
-    tag=$(curl --silent "https://api.github.com/repos/${1}/releases/latest" | # Get latest release from GitHub API
-          grep '"tag_name":'                                              | # Get tag line
-          sed -E 's/.*"([^"]+)".*/\1/'                                    ) # Pluck JSON value
+    platform=${1}
+    arch=${2}
 
-    tag_data=$(curl --silent "https://api.github.com/repos/${1}/git/ref/tags/${tag}")
+    # Grab the first commit SHA since as this script assumes it will be the
+    # latest.
+    platform="win32"
+    arch="x64"
+    commit_id=$(curl --silent "https://update.code.visualstudio.com/api/commits/stable/${platform}-${arch}" | sed s'/^\["\([^"]*\).*$/\1/')
 
-    sha=$(echo "${tag_data}"           | # Get latest release from GitHub API
-          grep '"sha":'                | # Get tag line
-          sed -E 's/.*"([^"]+)".*/\1/' ) # Pluck JSON value
-
-    sha_type=$(echo "${tag_data}"           | # Get latest release from GitHub API
-          grep '"type":'                    | # Get tag line
-          sed -E 's/.*"([^"]+)".*/\1/'      ) # Pluck JSON value
-
-    if [ "${sha_type}" != "commit" ]; then
-        combo_sha=$(curl -s "https://api.github.com/repos/${1}/git/tags/${sha}" | # Get latest release from GitHub API
-              grep '"sha":'                                                     | # Get tag line
-              sed -E 's/.*"([^"]+)".*/\1/'                                      ) # Pluck JSON value
-
-        # Remove the tag sha, leaving only the commit sha;
-        # this won't work if there are ever more than 2 sha,
-        # and use xargs to remove whitespace/newline.
-        sha=$(echo "${combo_sha}" | sed -E "s/${sha}//" | xargs)
-    fi
-
-    printf "${sha}"
+    printf "%s" "${commit_id}"
 }
 
-ARCH="x64"
-U_NAME=$(uname -m)
+PLATFORM="${1}"
+ARCH="${2}"
 
-if [ "${U_NAME}" = "aarch64" ]; then
-    ARCH="arm64"
+if [ -z "${PLATFORM}" ]; then
+    echo "please enter a platform, acceptable values are win32, linux, darwin, or alpine"
+    exit 1
 fi
 
-archive="vscode-server-linux-${ARCH}.tar.gz"
-owner='microsoft'
-repo='vscode'
-commit_sha=$(get_latest_release "${owner}/${repo}")
+if [ -z "${ARCH}" ]; then
+    U_NAME=$(uname -m)
+
+    if [ "${U_NAME}" = "aarch64" ]; then
+        ARCH="arm64"
+    elif [ "${U_NAME}" = "x86_64" ]; then
+        ARCH="x64"
+    elif [ "${U_NAME}" = "armv7l" ]; then
+        ARCH="armhf"
+    fi
+fi
+
+commit_sha=$(get_latest_release "${PLATFORM}" "${ARCH}")
 
 if [ -n "${commit_sha}" ]; then
     echo "will attempt to download VS Code Server version = '${commit_sha}'"
 
+    prefix="server-${PLATFORM}"
+    if [ "${PLATFORM}" = "alpine" ]; then
+        prefix="cli-${PLATFORM}"
+    fi
+
+    archive="vscode-${prefix}-${ARCH}.tar.gz"
     # Download VS Code Server tarball to tmp directory.
-    curl -L "https://update.code.visualstudio.com/commit:${commit_sha}/server-linux-${ARCH}/stable" -o "/tmp/${archive}"
+    curl -L "https://update.code.visualstudio.com/commit:${commit_sha}/${prefix}-${ARCH}/stable" -o "/tmp/${archive}"
 
     # Make the parent directory where the server should live.
     # NOTE: Ensure VS Code will have read/write access; namely the user running VScode or container user.
@@ -60,6 +77,8 @@ if [ -n "${commit_sha}" ]; then
 
     # Extract the tarball to the right location.
     tar --no-same-owner -xzv --strip-components=1 -C ~/.vscode-server/bin/"${commit_sha}" -f "/tmp/${archive}"
+    # Add symlink
+    cd ~/.vscode-server/bin && ln -s "${commit_sha}" default_version
 else
     echo "could not pre install vscode server"
 fi
